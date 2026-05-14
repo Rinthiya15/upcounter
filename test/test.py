830a8eda99bda921fad0f015dff73183a6aa9255
Rinthiya15 <rinthiya1508@gmail.com>
2026-05-14 10:01:35 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,46 +1,24 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
 @cocotb.test()
 async def test_upcounter(dut):
 
-    dut._log.info("Starting Up Counter Test")
+    cocotb.start_soon(Clock(dut.clk, 10, units="us").start())
 
-    # Create 100 KHz clock
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
-    # Initialize inputs
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
 
-    # Apply reset
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 5)
+    await ClockCycles(dut.clk, 2)
 
-    # Release reset
     dut.rst_n.value = 1
 
-    dut._log.info("Reset released")
+    expected = 0
 
-    # Check counter increments
-    for expected_count in range(10):
-
+    for _ in range(10):
         await ClockCycles(dut.clk, 1)
-
-        actual_count = int(dut.uo_out.value)
-
-        dut._log.info(
-            f"Expected={expected_count + 1}, Actual={actual_count}"
-        )
-
-        assert actual_count == (expected_count + 1), \
-            f"Counter mismatch: expected {expected_count + 1}, got {actual_count}"
-
-    dut._log.info("Up Counter Test Passed")
+        expected += 1
+        assert int(dut.uo_out.value) == expected
